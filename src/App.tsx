b235a0cad045fdd801296de03803fed6611a7f07
Fahad Bin Javed <126807388+Fahad-Javed20@users.noreply.github.com>
2025-10-25 11:20:32 +0500

    make changes

--- src/App.tsx
+++ src/App.tsx
@@ -4,38 +4,37 @@ import ProfileComponent from "./components/ProfileComponent";
 function App() {
   return (
     <>
-          <div className="flex gap-5">
-      <ProfileComponent
-        image={
-          "https://images.unsplash.com/photo-1534528741775-53994a69daeb?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fHByb2ZpbGV8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&q=60&w=500"
-        }
-        name={"Emily Chin"}
-        discription={
-          "   Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ducimus inventore facere autem odit! Aliquid"
-        }
- />
-
-  <ProfileComponent
-        image={
-          "https://images.unsplash.com/photo-1544723795-3fb6469f5b39?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mjd8fHByb2ZpbGV8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&q=60&w=500"
-        }
-        name={"Michael Lee"}
-        discription={
-          "   Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ducimus inventore facere autem odit! Aliquid"
-        }
+      <div className="flex gap-5">
+        <ProfileComponent
+          image={
+            "https://images.unsplash.com/photo-1534528741775-53994a69daeb?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fHByb2ZpbGV8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&q=60&w=500"
+          }
+          name={"Emily Chin"}
+          discription={
+            "   Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ducimus inventore facere autem odit! Aliquid"
+          }
         />
 
+        <ProfileComponent
+          image={
+            "https://images.unsplash.com/photo-1544723795-3fb6469f5b39?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mjd8fHByb2ZpbGV8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&q=60&w=500"
+          }
+          name={"Michael Lee"}
+          discription={
+            "   Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ducimus inventore facere autem odit! Aliquid"
+          }
+        />
 
-  <ProfileComponent
-        image={
-          "https://images.unsplash.com/photo-1503235930437-8c6293ba41f5?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NTJ8fHByb2ZpbGV8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&q=60&w=500"
-        }
-        name={"Sarah Davis"}
-        discription={
-          "   Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ducimus inventore facere autem odit! Aliquid"
-        }
- />
-        </div>
+        <ProfileComponent
+          image={
+            "https://images.unsplash.com/photo-1503235930437-8c6293ba41f5?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NTJ8fHByb2ZpbGV8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&q=60&w=500"
+          }
+          name={"Sarah Davis"}
+          discription={
+            "   Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ducimus inventore facere autem odit! Aliquid"
+          }
+        />
+      </div>
     </>
   );
 }
