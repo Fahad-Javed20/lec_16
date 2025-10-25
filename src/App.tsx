5a3b222b77edb7433b50394ded0aa1d6e3a86770
Fahad Bin Javed <126807388+Fahad-Javed20@users.noreply.github.com>
2025-10-25 15:52:07 +0500

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,112 @@
+import { useState } from "react";
 import "./App.css";
 import ProfileComponent from "./components/ProfileComponent";
 import type { UserType } from "./components/types/userType";
 
-function App() {
-  const user: UserType[] = [
-    {
-      image:
-        "https://images.unsplash.com/photo-1534528741775-53994a69daeb?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fHByb2ZpbGV8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&q=60&w=500",
-      name: "Emily Chin",
-      description:
-        "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ducimus inventore facere autem odit! Aliquid",
-      views: 1.6,
-      comments: 12,
-    },
-    {
-      image:
-        "https://images.unsplash.com/photo-1544723795-3fb6469f5b39?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mjd8fHByb2ZpbGV8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&q=60&w=500",
-      name: "Michael Lee",
-      description:
-        "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ducimus inventore facere autem odit! Aliquid",
-      views: 2.8,
-      comments: 23,
-    },
-    {
-      image:
-        "https://images.unsplash.com/photo-1503235930437-8c6293ba41f5?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NTJ8fHByb2ZpbGV8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&q=60&w=500",
-      name: "Sarah Davis",
-      description:
-        "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ducimus inventore facere autem odit! Aliquid",
-      views: 9.9,
-      comments: 12,
-    },
-  ];
+const usersArray: UserType[] = [
+  {
+    id: 1,
+    image:
+      "https://images.unsplash.com/photo-1534528741775-53994a69daeb?ixlib=rb-4.1.0&auto=format&fit=crop&q=60&w=500",
+    name: "Emily Chin",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ducimus inventore facere autem odit! Aliquid",
+    views: 1.6,
+    comments: 12,
+  },
+  {
+    id: 2,
+    image:
+      "https://images.unsplash.com/photo-1544723795-3fb6469f5b39?ixlib=rb-4.1.0&auto=format&fit=crop&q=60&w=500",
+    name: "Michael Lee",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ducimus inventore facere autem odit! Aliquid",
+    views: 2.8,
+    comments: 23,
+  },
+  {
+    id: 3,
+    image:
+      "https://images.unsplash.com/photo-1503235930437-8c6293ba41f5?ixlib=rb-4.1.0&auto=format&fit=crop&q=60&w=500",
+    name: "Sarah Davis",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ducimus inventore facere autem odit! Aliquid",
+    views: 9.9,
+    comments: 12,
+  },
+];
 
+function App() {
+  const [users, setUsers] = useState<UserType[]>(usersArray);
+
+  function addUser(newUser: UserType): void {
+    setUsers((prevUsers) => [...prevUsers, newUser]);
+  }
+
+  const newUser: UserType = {
+    id: users.length + 1,
+    image:
+      "https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-4.1.0&auto=format&fit=crop&q=60&w=500",
+    name: "New User",
+    description:
+      "This is a new user added dynamically to the list.This is a new user added dynamically to the list.",
+    views: 3.5,
+    comments: 45,
+  };
+
+  function addUserAtBegining(newUser1: UserType): void {
+    setUsers((prevUsers) => [newUser1, ...prevUsers]);
+  }
+
+  const newUser1: UserType = {
+    id: users.length + 1,
+    image:
+      "https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-4.1.0&auto=format&fit=crop&q=60&w=500",
+    name: "New User",
+    description:
+      "This is a new user added dynamically to the list.This is a new user added dynamically to the list.",
+    views: 3.5,
+    comments: 45,
+  };
+  function removeLastUser(): void {
+    setUsers((prevUsers) => prevUsers.slice(0, -1));
+  }
+
+  function removeFirstUser(): void {
+    setUsers((prevUsers) => prevUsers.slice(1));
+  }
   return (
     <>
-      <div className="flex gap-5">
-        <ProfileComponent user = { user[0]} />
-        <ProfileComponent user = { user[1]} />
-        <ProfileComponent user = { user[2]} />
+      <div className="flex gap-2 flex-wrap">
+        {users.map((user) => (
+          <ProfileComponent key={user.id} users={user} />
+        ))}
       </div>
+
+      <button
+        onClick={() => addUser(newUser)}
+        className="bg-blue-600 text-white px-6 py-2 rounded mt-5 ml-3"
+      >
+        Add The End
+      </button>
+      <button
+        onClick={() => addUserAtBegining(newUser1)}
+        className="bg-blue-600 text-white px-6 py-2 rounded mt-5 ml-3"
+      >
+        Add in Begining
+      </button>
+      <button
+        onClick={() => removeLastUser()}
+        className="bg-blue-600 text-white px-6 py-2 rounded mt-5 ml-3"
+      >
+        Remove End
+      </button>
+      <button
+        onClick={() => removeFirstUser()}
+        className="bg-blue-600 text-white px-6 py-2 rounded mt-5 ml-3"
+      >
+        Remove Begining
+      </button>
     </>
   );
 }
--- src/components/types/userType.ts
+++ src/components/types/userType.ts
@@ -1,4 +1,5 @@
 export type UserType = {
+  id:number;
   image: string;
   name: string;
   description: string;
